47bad732e7fda09fe1754df6c383f262bbda8890
Cam Chambers <cam_chambers@outlook.com>
2021-12-05 08:32:33 -0500

    Created a star rating componment

---
+++ src/StarRating.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Star from  './Star';
+
+function StarRating() {
+    return (
+        <div id='star-rating'>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+        </div>
+    )
+}
+
+export default StarRating;
